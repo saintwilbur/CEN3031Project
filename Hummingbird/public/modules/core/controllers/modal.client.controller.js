1f14e33f0552e984c868930120c78168ee1f5c9e
sltclss <sltclss@gmail.com>
2014-10-05 14:49:19 -0500

    New project commit using yo generator without Articles.

---
+++ Hummingbird/public/modules/core/controllers/modal.client.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('core').controller('ModalController', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size, tmplt) {
+
+    var modalInstance = $modal.open({
+      templateUrl: tmplt,
+      controller: 'ModalInstanceController',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+angular.module('core').controller('ModalInstanceController', function ($scope, $modalInstance) {
+
+
+  $scope.ok = function () {
+    $modalInstance.close('close');
+  };
+
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
