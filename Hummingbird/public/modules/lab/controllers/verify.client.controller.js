a0d744cc37e85770ba480d31e8bfebc755d8c65b
Elliot Schoenfeld <jeofner@gmail.com>
2014-10-29 16:27:58 -0400

    Added Verify HTML and controller
    Contains search function to filter the verify results.

---
+++ Hummingbird/public/modules/lab/controllers/verify.client.controller.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('lab').controller('VerifyController', ['$scope', 'Authentication','$http', 
+	function($scope, Authentication, $http) {
+			$scope.authentication = Authentication;
+			$scope.orders = [];
+			$scope.searchOrders = [];
+			//getOrders to verify  
+			$scope.searchOrders = function() {
+				
+				$http.get('/order/verify',{user: $scope.authentication.user._id}).success(function(response) 
+				{
+					$scope.searchOrders=response;
+					return $scope.searchOrders;
+				}).error(function(response) 
+				{
+					$scope.error = response.message;
+				}); 
+			};
+			$scope.getPendingOrders = function()
+				{
+					$http.get('/order/pending',{user: $scope.authentication.user._id}).success(function(response) 
+					{
+						$scope.orders=response;
+						return $scope.orders;
+					}).error(function(response) 
+					{
+						$scope.error = response.message;
+					}); 
+				};
+					$scope.$on('refreshOrders', function (event) {
+					$scope.getPendingOrders();
+				});
+				
+				$scope.searchOrder = function()
+				{
+					//var fieldArray = [$scope.formData.field1, $scope.formData.field2, $scope.formData.field3];
+
+					var search = 
+					{
+						ID: $scope.search
+					};
+					$http.post('/order/verify', search).success(function(response) {
+						$rootScope.$broadcast('refreshOrders');
+						$scope.search={};
+						 alert('You have narrrowed down your list of IDs');
+					}).error(function(response) {
+						$scope.error = response.message;
+					});
+				};
+				
+				
+			}
+		]);
