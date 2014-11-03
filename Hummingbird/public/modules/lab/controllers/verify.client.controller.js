fec670fe099166b85aa88c7348a8ff36b252e828
davidgithub614 <davidy114@hotmail.com>
2014-11-03 15:54:01 -0500

    General Commit: Minor bug fixed

--- Hummingbird/public/modules/lab/controllers/verify.client.controller.js
+++ Hummingbird/public/modules/lab/controllers/verify.client.controller.js
@@ -3,16 +3,16 @@
 angular.module('lab').controller('VerifyController', ['$scope', '$rootScope', 'Authentication','$http', 
 	function($scope, $rootScope, Authentication, $http) {
 			$scope.authentication = Authentication;
-<<<<<<< Updated upstream
+
 			$scope.results = 
 			[
 				/*{orderId: '2345fsd', date: 'dateHere' },
 				{orderId: '123asdfw235245', date: 'dateHere' },
 				{orderId: '24y4h5fa', date: 'dateHere' }*/
 			];
-=======
+
 			$scope.orders = [{orderId: '2231', date: '231341'},{orderId: '4598', date: '120931'}];
->>>>>>> Stashed changes
+
 			$scope.searchOrders = [];
 			//getOrders to verify  
 			$scope.searchOrders = function() {
