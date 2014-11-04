ab2a6c2e22919b5deb87c78f54a1f824f6a305b5
Olguy <djcaneus@yahoo.fr>
2014-11-04 11:18:26 -0600

    Remove and clean up files containing JsFiddle which we are no longer using --jcaneus

--- Hummingbird/public/modules/lab/tests/verify.client.controller.test.js
+++ Hummingbird/public/modules/lab/tests/verify.client.controller.test.js
@@ -50,9 +50,9 @@
 			});
 		}));
 
-		it('Should do some controller test', inject(function() {
+		it('Should search order for user', inject(function() {
 			// The test logic
-			// ...
+			
 		}));
 	});
 }());
