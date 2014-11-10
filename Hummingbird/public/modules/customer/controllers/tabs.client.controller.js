ef06822a962402bfdb2214887931a6c113551cd5
Olguy <djcaneus@yahoo.fr>
2014-11-10 16:04:01 -0600

    "Clean up and remove non working xeditable codes on the customer dashboard --jcaneus1"

--- Hummingbird/public/modules/customer/controllers/tabs.client.controller.js
+++ Hummingbird/public/modules/customer/controllers/tabs.client.controller.js
@@ -4,8 +4,8 @@ angular.module('customer').controller('TabsDemoCtrl', function ($scope) {
 	$scope.tabs = [
 	    { title:'New Order', content:'/modules/customer/views/new-order.client.view.html' },
 	    { title:'Pending Orders', content:'/modules/customer/views/pending.client.view.html' },
-	    { title:'Order History', content:'' },
-	    { title:'Medical Background', content:'/modules/customer/views/history.client.view.html' }
+	    { title:'Order History', content:'/modules/customer/views/history.client.view.html' },
+	    { title:'Medical Background', content:'/modules/customer/views/medicalHistory.client.view.html' }
 	    //{ title:'Nancy', content:'/modules/customer/views/medicalHistory.client.view.html' }
 	  ];
 	  var test = true;
