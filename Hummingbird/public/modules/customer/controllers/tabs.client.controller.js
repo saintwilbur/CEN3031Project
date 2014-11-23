b4093ffcc0daa7e40f324011646e690e3bb89e2e
gandhima24 <monicagandhi11@ymail.com>
2014-11-22 21:36:49 -0800

    some major changes in customer page view and addding a order button on customer splash page which will functions as new order

--- Hummingbird/public/modules/customer/controllers/tabs.client.controller.js
+++ Hummingbird/public/modules/customer/controllers/tabs.client.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('customer').controller('TabsDemoCtrl', function ($scope) {
 	$scope.tabs = [
-	    { title:'New Order', content:'/modules/customer/views/new-order.client.view.html' },
-	    { title:'Pending Orders', content:'/modules/customer/views/pending.client.view.html' },
+	  //  { title:'New Order', content:'/modules/customer/views/new-order.client.view.html' },
+	   // { title:'Pending Orders', content:'/modules/customer/views/pending.client.view.html' },
 	    { title:'Order History', content:'/modules/customer/views/history.client.view.html' },
-	    { title:'Medical Background', content:'/modules/customer/views/medicalHistory.client.view.html' },
+	 
 	    { title:'Register-Kit', content:'/modules/customer/views/register-kit.client.view.html' }
 	  ];
 	  var test = true;
